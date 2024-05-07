da8c7ce678be969d1742bcaffb395322e77e8317
Luís Arteiro <lucho14garfield@gmail.com>
2024-05-07 21:20:14 +0100

    chore: Barebones input working on both platforms.

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -14,68 +14,113 @@ interface Config {
 
 	lineCount: number;
 	filetypes?: string[];
+	clickCallback?: (event: MouseEvent, input: HTMLInputElement) => void;
 }
 
 export class FilePluginController implements Controller<FilePluginView> {
 	public readonly value: Value<File | null>;
-	private readonly filetypes?: string[];
-
 	public readonly view: FilePluginView;
 	public readonly viewProps: ViewProps;
 
 	constructor(doc: Document, config: Config) {
-		// Binding click event handlers
-		this.onInputClick = this.onInputClick.bind(this);
-		this.onDeleteButtonClick_ = this.onDeleteButtonClick_.bind(this);
-
-		// Receive the bound value from the plugin
 		this.value = config.value;
-
-		// Get filetypes
-		this.filetypes = config.filetypes;
-
-		// and also view props
 		this.viewProps = config.viewProps;
-		this.viewProps.handleDispose(() => {
-			// Called when the controller is disposing
-		});
-
-		// Create a custom view
 		this.view = new FilePluginView(doc, {
-			value: this.value,
 			viewProps: this.viewProps,
+			value: config.value,
 			lineCount: config.lineCount,
-			filetypes: config.filetypes
+			filetypes: config.filetypes,
+			clickCallback: config.clickCallback,
 		});
 
-		// You can use `PointerHandler` to handle pointer events in the same way as Tweakpane do
-		const containerPtHandler = new PointerHandler(this.view.container);
-		containerPtHandler.emitter.on('down', this.onInputClick);
+		this.onFile = this.onFile.bind(this);
+		this.onDrop = this.onDrop.bind(this);
+		this.onDragStart = this.onDragStart.bind(this);
+		this.onDragOver = this.onDragOver.bind(this);
+		this.onDragLeave = this.onDragLeave.bind(this);
 
-		const deleteButtonPTHandler = new PointerHandler(this.view.deleteButton);
-		deleteButtonPTHandler.emitter.on('down', this.onDeleteButtonClick_);
+		this.view.input.addEventListener('change', this.onFile);
+		this.view.element.addEventListener('drop', this.onDrop);
+		this.view.element.addEventListener('dragstart', this.onDragStart);
+		this.view.element.addEventListener('dragover', this.onDragOver);
+		this.view.element.addEventListener('dragleave', this.onDragLeave);
+
+		this.viewProps.handleDispose(() => {
+			this.view.input.removeEventListener('change', this.onFile);
+			this.view.element.removeEventListener('drop', this.onDrop);
+			this.view.element.removeEventListener('dragstart', this.onDragStart);
+			this.view.element.removeEventListener('dragover', this.onDragOver);
+			this.view.element.removeEventListener('dragleave', this.onDragLeave);
+		});
+
+		this.value.emitter.on('change', () => this.handleValueChange());
+
+		this.handleValueChange();
 	}
 
-	/**
-	 * Event handler when the container HTML element is clicked.
-	 * It checks if the filetype is valid and if the user has chosen a file.
-	 * If the file is valid, the `rawValue` of the controller is set.
-	 * @param ev Pointer event.
-	 */
-	private onInputClick(_ev?: PointerHandlerEvent) {
+	private onFile(event: Event): void {
+		const files = (event?.target as HTMLInputElement).files;
+		if (!files || !files.length) return;
 
+		const file = files[0];
+		this.setValue(file);
+		// this.updateImage(url);
 	}
 
-	/**
-	 * Event handler when the delete HTML button is clicked.
-	 * It resets the `rawValue` of the controller.
-	 * @param ev Pointer event.
-	 */
-	private onDeleteButtonClick_(_ev: PointerHandlerEvent) {
-		const file = this.value.rawValue;
-
-		if (file) {
-			this.value.setRawValue(null);
+	private onDrop(event: DragEvent) {
+		event.preventDefault();
+		try {
+			const {dataTransfer} = event;
+			const file = dataTransfer?.files[0];
+			if (file) {
+				// const url = URL.createObjectURL(file);
+				// this.updateImage(url);
+				this.setValue(file);
+			} else {
+				const imgId = dataTransfer?.getData('img-id');
+				if (imgId) {
+					const img = document.getElementById(imgId) as HTMLImageElement;
+					this.setValue(img);
+				} else {
+					const url = dataTransfer?.getData('url');
+					if (!url) throw new Error('No url');
+					this.setValue(url);
+				}
+				// loadImage(url).then(async (image) => {
+				// 	console.log('drop', image);
+				// 	const clone = await cloneImage(image);
+				// 	// this.updateImage(clone.src);
+				// 	this.setValue(clone);
+				// });
+			}
+		} catch (e) {
+			console.error('Could not parse the dropped image', e);
+		} finally {
+			this.view.changeDraggingState(false);
 		}
+	}
+
+	private onDragStart(event: DragEvent) {
+
+	}
+
+	private onDragOver(event: Event) {
+		event.preventDefault();
+		this.view.changeDraggingState(true);
+	}
+
+	private onDragLeave() {
+		this.view.changeDraggingState(false);
+	}
+
+	private setValue(file: any) {
+
+	}
+
+	private handleValueChange() {
+	}
+
+	private handlePlaceholderImage(){
+
 	}
 }
--- src/view/view.ts
+++ src/view/view.ts
@@ -6,194 +6,42 @@ interface Config {
 
 	lineCount: number;
 	filetypes?: string[];
+	clickCallback?: (event: MouseEvent, input: HTMLInputElement) => void;
 }
 
 // Create a class name generator from the view name
 // ClassName('tmp') will generate a CSS class name like `tp-tmpv`
-const containerClassName = ClassName('ctn');
+const inputClassName = ClassName('ctn');
 const buttonClassName = ClassName('btn');
 
 export class FilePluginView implements View {
-	// Root element
 	public readonly element: HTMLElement;
-
-	// Root element's children
-	public readonly container: HTMLElement;
-	public readonly deleteButton: HTMLElement;
-
-	// Container element's children
-	private inputEl_: HTMLInputElement;
-	private fileIconEl_: HTMLElement;
-	private textEl_: HTMLElement;
-
-	// Value of the controller
-	private value_: Value<File | null>;
+	public readonly input: HTMLInputElement;
 
 	constructor(doc: Document, config: Config) {
-		// Binding event handlers
-		this.onDrop_ = this.onDrop_.bind(this);
 
-		// DOM --------------------------
 		this.element = doc.createElement('div');
+		this.element.classList.add(inputClassName());
+		config.viewProps.bindClassModifiers(this.element);
 
-		// Create container and children
-		this.inputEl_ = document.createElement('input');
-		this.inputEl_.id = 'file-input-el';
-		this.inputEl_.setAttribute('name', 'file');
-		this.inputEl_.setAttribute('type', 'file');
-		this.inputEl_.setAttribute(
+		this.input = doc.createElement('input');
+		this.input.setAttribute('type', 'file');
+		this.input.setAttribute(
 			'accept',
 			config.filetypes ? config.filetypes.join(',') : '*',
 		);
-		this.inputEl_.addEventListener('click', () => {});
-		this.inputEl_.addEventListener('touchstart', () => {});
-		this.inputEl_.addEventListener('touchend', () => {});
-		this.inputEl_.addEventListener('touchcancel', () => {});
-		this.inputEl_.addEventListener('touchmove', () => {});
-		this.inputEl_.addEventListener(
-			'input',
-			(_ev) => {
+		this.input.style.height = `calc(20px * ${config.lineCount})`;
 
-				console.log(this.inputEl_.files)
-				
-				// Check if user has chosen a file
-				if (this.inputEl_.files && this.inputEl_.files.length > 0) {
-					const file = this.inputEl_ .files[0];
-					const fileExtension =
-						'.' + file.name.split('.').pop()?.toLowerCase();
-
-					// Check if filetype is allowed
-					if (
-						config.filetypes &&
-						config.filetypes.length > 0 &&
-						!config.filetypes.includes(fileExtension) &&
-						fileExtension
-					) {
-						return;
-					} else {
-						this.value_.setRawValue(file);
-					}
-				}
-			},
-			{once: true},
-		);
-		this.inputEl_ .style.cursor = 'pointer';
-		this.inputEl_ .style.opacity = '100';
-		this.inputEl_ .style.width = '100%';
-		this.inputEl_ .style.height = '100%';
-		this.inputEl_ .style.position = 'absolute';
-
-		this.container = doc.createElement('div');
-		this.container.style.height = `calc(20px * ${config.lineCount})`;
-		this.container.style.cursor = 'pointer';
-		this.container.classList.add(containerClassName());
-		this.container.appendChild(this.inputEl_ );
-
-		this.element.appendChild(this.container);
-
-		this.fileIconEl_ = doc.createElement('div');
-		this.fileIconEl_.classList.add(containerClassName('icon'));
-		this.container.appendChild(this.fileIconEl_);
-
-		this.textEl_ = doc.createElement('span');
-		this.textEl_.classList.add(containerClassName('text'));
-
-		// Create delete button
-		this.deleteButton = doc.createElement('button');
-		this.deleteButton.classList.add(buttonClassName('b'));
-		this.deleteButton.innerHTML = 'Delete';
-		this.deleteButton.style.display = 'none';
-		this.element.appendChild(this.deleteButton);
-
-		// Add drag and drop event handlers
-		this.element.addEventListener('drop', this.onDrop_);
-		this.element.addEventListener('dragover', this.onDragOver_);
-
-		// Events ------------------------
-		// Receive the bound value from the controller
-		this.value_ = config.value;
-		// Handle 'change' event of the value
-		this.value_.emitter.on('change', this.onValueChange_.bind(this));
-
-		// Bind view props to the element
-		config.viewProps.bindClassModifiers(this.element);
-
-		// View dispose handler
-		config.viewProps.handleDispose(() => {
-			// Called when the view is disposing
-			console.log('TODO: dispose view');
-		});
-	}
-
-	/**
-	 * Called when a `dragover` event is created.
-	 * It simply prevents files being opened when these events are occuring.
-	 * @param ev Drag event object.
-	 */
-	onDragOver_(ev: DragEvent) {
-		ev.preventDefault();
-	}
-
-	/**
-	 * Called whenever a `drop` event is created.
-	 * It changes the `rawValue` of the controller with the file that was dropped.
-	 * @param ev Event object.
-	 */
-	onDrop_(ev: Event) {
-		if (ev instanceof DragEvent) {
-			// Prevent default behavior (Prevent file from being opened)
-			ev.preventDefault();
-
-			if (ev.dataTransfer) {
-				if (ev.dataTransfer.files) {
-					// We only change the value if the user has dropped a single file
-					const filesArray = [ev.dataTransfer.files][0];
-					if (filesArray.length == 1) {
-						const file = filesArray.item(0);
-						this.value_.setRawValue(file);
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Called when the value (bound to the controller) changes (e.g. when the file is selected).
-	 */
-	private onValueChange_() {
-		const fileObj = this.value_.rawValue;
-
-		// If there's a file selected, we show its contents
-		if (fileObj) {
-			// Setting the text of the file to the element
-			this.textEl_.textContent = fileObj.name;
-
-			// Removing icon and adding text
-			this.container.appendChild(this.textEl_);
-			if (this.container.contains(this.fileIconEl_)) {
-				this.container.removeChild(this.fileIconEl_);
-			}
-
-			// Adding button to delete
-			this.deleteButton.style.display = 'block';
-			this.container.style.border = 'unset';
-		} 
 		
-		// Otherwise, we reset the input
-		else {
-			// Setting the text of the file to the element
-			this.textEl_.textContent = '';
+		this.element.appendChild(this.input);
+	}
 
-			// Resetting input value
-			this.inputEl_.value = '';
-			this.value_.setRawValue(null); // TODO move things to controller
-
-			// Removing text and adding icon
-			this.container.appendChild(this.fileIconEl_);
-			this.container.removeChild(this.textEl_);
-
-			this.deleteButton.style.display = 'none';
-			this.container.style.border = '1px dashed #717070';
+	changeDraggingState(state: boolean) {
+		const el = this.element;
+		if (state) {
+			el?.classList.add(inputClassName('area_dragging'));
+		} else {
+			el?.classList.remove(inputClassName('area_dragging'));
 		}
 	}
 }
